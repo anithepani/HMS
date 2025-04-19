6c6b313b79b49d8952bc89a351269b36a7eaab2d
anithepani <aryanzia2005@gmail.com>
2025-04-19 17:08:56 +0500

    Initializing the Project

--- trial.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class trial{
-    static final Scanner in = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
